ce22aecd6e4d44c0afdd970aa3e7ef2dd8563329
gusdev0258 <16326176760@edu.udesc.br>
2023-10-01 18:36:46 -0300

    create user and comments module and conect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    CommentsModule,
+    MongooseModule.forRoot(
+      'mongodb://root:root@localhost:27017/threads_db?authSource=admin',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
